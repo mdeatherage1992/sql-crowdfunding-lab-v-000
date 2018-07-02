c9fc57ad89fc7faf5bf5f386fe2fb88c203a74b9
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-02 17:57:37 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE projects(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
